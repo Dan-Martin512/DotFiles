57dd7a4271b0202ce798eb9bbfd9d3f444380ae8
Dan Martin <dan.512@hotmail.co.uk>
2020-04-24 15:36:12 +0100

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,7 @@
+powerline-daemon -q
+alias timer='termdown -b'
+set POWERLINE_BASH_CONTINUATION 1
+set POWERLINE_BASH_SELECT 1
+set fish_function_path $fish_function_path "/usr/lib/python3.8/site-packages/powerline/bindings/fish"
+powerline-setup
+/opt/shell-color-scripts/colorscripts/arch
